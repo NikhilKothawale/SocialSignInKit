4875ce51fd7c7f252c552b7f4bda100e8e7d0b31
Nikhil Kothawale <nikhil.kothawale@indexnine.com>
2020-08-16 23:54:28 +0530

    Google Sign-In added.

---
+++ SocialSignInKit/GoogleSignIn/GoogleService.swift
@@ -0,0 +1,36 @@
+//
+//  GoogleService.swift
+//  SocialSignInKit
+//
+//  Created by Indexnine on 15/08/20.
+//  Copyright © 2020 Nikhil Kothawale. All rights reserved.
+//
+
+import Foundation
+
+class GoogleService{
+    
+    var dict = [String: AnyObject]()
+    static var singleInstance : GoogleService?
+    
+    static var sharedInstance : GoogleService{
+        get {
+            if singleInstance == nil{
+                singleInstance = GoogleService()
+            }
+            return singleInstance!
+        }
+    }
+    
+    private init() {
+        if let path = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"), let dict = NSDictionary(contentsOfFile: path) as? [String: AnyObject] {
+            self.dict = dict
+        }
+    }
+    
+    var clientID : String{
+        get {
+            return dict["CLIENT_ID"] as! String
+        }
+    }
+}
---
+++ SocialSignInKit/GoogleSignIn/GoogleUser.swift
@@ -0,0 +1,54 @@
+//
+//  GoogleUser.swift
+//  SocialSignInKit
+//
+//  Created by Indexnine on 15/08/20.
+//  Copyright © 2020 Nikhil Kothawale. All rights reserved.
+//
+
+import Foundation
+
+public class GoogleUser {
+    
+    var _userId : String = ""
+    var _idToken : String?
+    var _fullName : String?
+    var _givenName : String?
+    var _familyName : String?
+    var _email : String?
+    
+    open var userId : String {
+        return _userId
+    }
+    
+    open var idToken : String? {
+        return _idToken
+    }
+    
+    open var fullName : String? {
+        return _fullName
+    }
+    
+    open var givenName : String? {
+        return _givenName
+    }
+    
+    open var familyName : String? {
+        return _familyName
+    }
+    
+    open var email : String? {
+        return _email
+    }
+    
+    init(userId: String, idToken: String?,  fullName: String?, givenName: String?, familyName: String?, email: String?) {
+        self._userId = userId
+        self._idToken = idToken
+        self._fullName = fullName
+        self._givenName = givenName
+        self._familyName = familyName
+        self._email  = familyName
+        
+    }
+    
+}
