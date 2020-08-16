cacb034361c0fc94d2ad4ffa78aca4e692da19ab
Nikhil Kothawale <nikhil.kothawale@indexnine.com>
2020-08-16 23:35:45 +0530

    Initial project setup

---
+++ SocialSignInKit/SocialSignInKit.h
@@ -0,0 +1,19 @@
+//
+//  SocialSignInKit.h
+//  SocialSignInKit
+//
+//  Created by Indexnine on 15/08/20.
+//  Copyright © 2020 Nikhil Kothawale. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SocialSignInKit.
+FOUNDATION_EXPORT double SocialSignInKitVersionNumber;
+
+//! Project version string for SocialSignInKit.
+FOUNDATION_EXPORT const unsigned char SocialSignInKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SocialSignInKit/PublicHeader.h>
+
+
